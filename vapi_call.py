c05da899cacf99b7cefa13600656ace185143c1f
Jasper Morgal <jpmorgal8@gmail.com>
2025-10-25 02:02:37 -0700

    Vapi api updates

--- vapi_call.py
+++ vapi_call.py
@@ -2,11 +2,12 @@ from vapi import Vapi
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from create_mock_data import build_system_prompt, select_mock_data
 
 client = Vapi(token=os.getenv('VAPI_API_KEY'))
 
 first_message = "Hello, this is a test call with an ai agent."
-system_prompt = "Respond to the user's message with a friendly greeting and a brief introduction to the agent."
+system_prompt = build_system_prompt(select_mock_data(1))
 agent_id = "6f723c39-5410-4b55-977f-ad80a1b947be"
 
 client.assistants.update(
@@ -21,7 +22,7 @@ client.assistants.update(
 
 call = client.calls.create(
     assistant_id=agent_id,
-    phone_number_id="f87d0e5f-73b5-4b44-bc98-23aa94b783f8",
+    phone_number_id="1c14896d-6c86-4014-b9de-b5bc873c059b",
     customer={"number": "+18589222159"},
 )
 print(call.id)
