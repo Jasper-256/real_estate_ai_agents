a98192a4827baae45f198629879ee0d40fd0b703
Jasper Morgal <jpmorgal8@gmail.com>
2025-10-26 08:17:02 -0700

    Fixes to run all

--- agents/run_all_agents.py
+++ agents/run_all_agents.py
@@ -14,9 +14,9 @@ from real_estate_intern import agent as real_estate_intern
 from local_discovery_agent import agent as local_discovery_agent
 from mapbox_agent import agent as mapbox_agent
 from research_agent import agent as research_agent
-from main_sandbox.main import coordinator as real_estate_agent
 from prober_agent import agent as prober_agent
 from vapi_agent import agent as vapi_agent
+from main_sandbox.main import coordinator as real_estate_agent
 
 if __name__ == "__main__":
     # Create a Bureau to run all agents concurrently
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     bureau.add(local_discovery_agent)
     bureau.add(mapbox_agent)
     bureau.add(research_agent)
-    bureau.add(real_estate_agent)
     bureau.add(prober_agent)
     bureau.add(vapi_agent)
-    
+    bureau.add(real_estate_agent)
+
     # Run all agents concurrently
     bureau.run()
